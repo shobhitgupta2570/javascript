e9fcdf87a14dedbc4807b6a5610e2f46e9e1ea11
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-15 17:13:30 +0530

    class instance and functions

--- oops/class.js
+++ oops/class.js
@@ -3,7 +3,11 @@ class Person {
     this.name = name;
     this.age = age;
   }
-
-  
+  displayInfo(){
+    console.log(`${this.name}'s age is ${this.age}`)
+  }
 }
 
+const Person1 = new Person("Shobhit",23);
+
+Person1.displayInfo();
