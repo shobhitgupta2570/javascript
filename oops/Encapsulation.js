d97ea0f4eba9678424603a35cf940f78f2dfffb7
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-20 18:03:50 +0530

    example encapsulatiion

--- oops/Encapsulation.js
+++ oops/Encapsulation.js
@@ -1,2 +1,19 @@
 // ncapsulation is the concept of wrapping data and methods that operate on the data within a single unit. This is typically achieved by using classes and making properties private using the # syntax (introduced in ECMAScript 2021):
 
+class Person{
+    #name
+    #age
+
+    constructor(name,age){
+        this.#name = name;
+        this.#age = age;
+    }
+
+    displayInfo(){
+        console.log(`${this.#name} has ${this.#age}`)
+    }
+}
+
+const person1 = new Person("Shobhit",23);
+person1.displayInfo();
+// console.log(person1.#name);
