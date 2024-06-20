09be5e2f7230c9fbf4a8abfa414cd5821d816a61
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-20 17:52:55 +0530

    Encapsulation

---
+++ oops/Encapsulation.js
@@ -0,0 +1,1 @@
+// ncapsulation is the concept of wrapping data and methods that operate on the data within a single unit. This is typically achieved by using classes and making properties private using the # syntax (introduced in ECMAScript 2021):
