14ceba369fe4458eddd5e9a84273258e87bb5a10
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-21 16:09:29 +0530

    Polymorphism

--- oops/Polymorphism.js
+++ oops/Polymorphism.js
@@ -1,1 +1,33 @@
 // Polymorphism allows methods to do different things based on the object it is acting upon.
+
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+
+    displayInfo(){
+        console.log(`${this.name} has ${this.age}`)
+    }
+}
+
+class Students extends Person{
+    constructor(name,age,grade){
+        super(name,age);
+        this.grade = grade;
+    }
+    displayInfo(){
+        super.displayInfo()
+        console.log(`${this.name} has ${this.age} got ${this.grade}`)
+    }
+}
+
+const people=[
+new Person("Shobhit",23),
+new Students("Jagdish",24,"A"),
+new Person("Nitish",25)
+]
+
+people.forEach((i)=>{
+    i.displayInfo();
+})
