364dfbf3f278608cf336879f576f3723cc713957
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-22 18:22:23 +0530

    Call Stack

--- EventLoop/EventLoop.js
+++ EventLoop/EventLoop.js
@@ -1,3 +1,11 @@
 // The event loop is a fundamental concept in JavaScript, particularly important for understanding how JavaScript handles asynchronous operations.
 // It enables non-blocking I/O operations, allowing JavaScript to perform other tasks while waiting for external
 //  operations (such as I/O, timers, or network requests) to complete.
+
+// How the Event Loop Works
+
+
+// Call Stack:
+
+// This is where JavaScript keeps track of what function is currently being executed and what functions are called from within that function.
+// The call stack operates on a last-in, first-out (LIFO) principle.
