24d7fc6a9f912394652731254f3ad1a003ed37f1
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-22 18:20:30 +0530

    EventLoop

---
+++ EventLoop/EventLoop.js
@@ -0,0 +1,3 @@
+// The event loop is a fundamental concept in JavaScript, particularly important for understanding how JavaScript handles asynchronous operations.
+// It enables non-blocking I/O operations, allowing JavaScript to perform other tasks while waiting for external
+//  operations (such as I/O, timers, or network requests) to complete.
