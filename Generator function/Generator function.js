2c3a71ed0b73440350fcbfb2a31d0e2b6789bb39
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-24 18:12:23 +0530

    Generaror function

---
+++ Generator function/Generator function.js
@@ -0,0 +1,7 @@
+// Generator functions in JavaScript are a special type of function that can be paused and resumed, 
+// allowing you to produce a sequence of values over time. They are useful for handling asynchronous operations, 
+// iterating over large data sets, or implementing lazy evaluation.
+
+// Syntax of Generator Functions
+// A generator function is defined using the function* syntax. Inside the function, the yield keyword is used to 
+// pause the function and return a value.
