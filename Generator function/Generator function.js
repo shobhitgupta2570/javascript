4eb6767a374cc06aea4d52d86644871c48417b81
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-28 17:39:30 +0530

    generator

--- Generator function/Generator function.js
+++ Generator function/Generator function.js
@@ -5,3 +5,18 @@
 // Syntax of Generator Functions
 // A generator function is defined using the function* syntax. Inside the function, the yield keyword is used to 
 // pause the function and return a value.
+
+function* simpleGenerator() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+  
+  // Using the generator function
+  const generator = simpleGenerator();
+  
+  console.log(generator.next()); // { value: 1, done: false }
+  console.log(generator.next()); // { value: 2, done: false }
+  console.log(generator.next()); // { value: 3, done: false }
+  console.log(generator.next()); // { value: undefined, done: true }
+  
